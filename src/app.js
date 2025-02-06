8b6b6046d35add2ea9f39eb9e2ad8ae798386852
Cheftonic <161465516+cheftonic92@users.noreply.github.com>
2025-02-06 11:51:06 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let who = ['The dog ', 'My grandma ', 'The mailman ', 'My bird '];
+  let action = ['ate ', 'peed ', 'crushed ', 'broke '];
+  let what = ['my homework ', 'my phone ', 'the car '];
+  let when = ['before the class!', 'when I was sleeping!', 'while I was exercising!', 'during my lunch!', 'while I was praying!'];
+
+  function randomizer(arr) {
+    return Math.floor(Math.random() * arr.length)
+  }
+
+  const excuse = who[randomizer(who)] +  
+  action[randomizer(action)] +  
+  what[randomizer(what)] +  
+  when[randomizer(when)];
+
+  document.querySelector("#excuse").innerHTML = excuse;
   console.log("Hello Rigo from the console!");
 };
